34df7061e1c46773895c08ce9e4b9655f639ebd8
Ignacio Velazquez <ivelazquez85@gmail.com>
2015-01-14 22:33:03 +0000

    Adding command options to avoid interaction

--- src/Nass600/Command/VhostCreateCommand.php
+++ src/Nass600/Command/VhostCreateCommand.php
@@ -5,6 +5,7 @@ namespace Nass600\Command;
 use Nass600\Builder\NginxBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
@@ -20,7 +21,32 @@ class VhostCreateCommand extends Command
     {
         $this
             ->setName("nass600:vhost:create")
-            ->setDescription("Creates an vhost for this project");
+            ->setDescription("Creates an vhost for this project")
+            ->addOption(
+                'server',
+                's',
+                InputOption::VALUE_OPTIONAL,
+                'Which server are you using?'
+            )
+            ->addOption(
+                'server-name',
+                'sn',
+                InputOption::VALUE_OPTIONAL,
+                'Which is the server name?'
+            )
+            ->addOption(
+                'document-root',
+                'dr',
+                InputOption::VALUE_OPTIONAL,
+                'Where is the project stored?'
+            )
+            ->addOption(
+                'vhost-filename',
+                'vf',
+                InputOption::VALUE_OPTIONAL,
+                'How would you like to name the vhost file?'
+            );
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -30,47 +56,59 @@ class VhostCreateCommand extends Command
         $helper = $this->getHelper('question');
 
         // Httpd server
-        $serverQuestion = new ChoiceQuestion(
-            '<info>Which server are you using? <comment>(default: nginx)</comment></info> ',
-            array('nginx', 'apache'),
-            0
-        );
-        $serverQuestion->setErrorMessage('Web server %s not supported.');
+        $config['server'] = $input->getOption('server');
+        if (null === $config['server']) {
+            $serverQuestion = new ChoiceQuestion(
+                '<info>Which server are you using? <comment>(default: nginx)</comment></info> ',
+                array('nginx', 'apache'),
+                0
+            );
+            $serverQuestion->setErrorMessage('Web server %s not supported.');
 
-        $config['server'] = $helper->ask($input, $output, $serverQuestion);
+            $config['server'] = $helper->ask($input, $output, $serverQuestion);
+        }
 
         // Server name
-        $serverNameQuestion = new Question('<info>Which is the server name?</info> ');
+        $config['serverName'] = $input->getOption('server-name');
+        if (null === $config['serverName']) {
+            $serverNameQuestion = new Question('<info>Which is the server name?</info> ');
 
-        $config['serverName'] = $helper->ask($input, $output, $serverNameQuestion);
+            $config['serverName'] = $helper->ask($input, $output, $serverNameQuestion);
+        }
 
         // Document root
-        $documentRootQuestion = new Question('<info>Where is the project stored?</info> ', 'hello');
+        $config['documentRoot'] = $input->getOption('document-root');
+        if (null === $config['documentRoot']) {
+            $documentRootQuestion = new Question('<info>Where is the project stored?</info> ', 'hello');
 
-        $documentRootQuestion->setValidator(function ($answer) {
-            if (!file_exists($answer)) {
-                throw new \RuntimeException(
-                    'The path you inserted does not exist'
-                );
-            }
-            if (!is_dir($answer)) {
-                throw new \RuntimeException(
-                    'The path you inserted is not a directory'
-                );
-            }
-            return $answer;
-        });
+            $documentRootQuestion->setValidator(function ($answer) {
+                if (!file_exists($answer)) {
+                    throw new \RuntimeException(
+                        'The path you inserted does not exist'
+                    );
+                }
+                if (!is_dir($answer)) {
+                    throw new \RuntimeException(
+                        'The path you inserted is not a directory'
+                    );
+                }
+                return $answer;
+            });
 
-        $config['documentRoot'] = $helper->ask($input, $output, $documentRootQuestion);
+            $config['documentRoot'] = $helper->ask($input, $output, $documentRootQuestion);
+        }
 
         // Vhost filename
-        $vhostFilenameQuestion = new Question(
-            "<info>How would you like to name the vhost file? " .
-            "<comment>(default: {$config['serverName']})</comment></info> ",
-            $config['serverName']
-        );
+        $config['vhostFilename'] = $input->getOption('vhost-filename');
+        if (null === $config['vhostFilename']) {
+            $vhostFilenameQuestion = new Question(
+                "<info>How would you like to name the vhost file? " .
+                "<comment>(default: {$config['serverName']})</comment></info> ",
+                $config['serverName']
+            );
 
-        $config['vhostFilename'] = $helper->ask($input, $output, $vhostFilenameQuestion);
+            $config['vhostFilename'] = $helper->ask($input, $output, $vhostFilenameQuestion);
+        }
 
         $builder = new NginxBuilder($config);
 
