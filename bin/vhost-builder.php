abc698a63a96beff5f24e66dd90153913a9017cf
Ignacio Velazquez <ivelazquez85@gmail.com>
2015-02-13 23:20:08 +0000

    Adding autoload path

--- bin/vhost-builder.php
+++ bin/vhost-builder.php
@@ -4,8 +4,10 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/../autoload.php')) {
     require __DIR__ . '/../autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require __DIR__ . '/../../../autoload.php';
 } else {
-    throw new Exception("sdasd");
+    throw new Exception("Autoload file not found");
 }
 
 $app = new Nass600\Tool\Application('Vhost Builder', '1.0.0');
